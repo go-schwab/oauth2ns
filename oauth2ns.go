539ecf4f2092d962717ac4a5a18a4ed09b8a565b
sam <hi@samjtro.com>
2024-11-06 10:42:41 -0600

    major: desc: - fixing errors

--- oauth2ns.go
+++ oauth2ns.go
@@ -192,7 +192,7 @@ func startHTTPServer(ctx context.Context, conf *oauth2.Config) (clientChan chan 
 
 	http.HandleFunc("/", callbackHandler(ctx, conf, clientChan))
 
-	srv := &http.Server{Addr: fmt.Sprintf("https://%s:%s", IP, PORT)}
+	srv := &http.Server{Addr: fmt.Sprintf("https://%s:%d", IP, PORT)}
 
 	// handle server shutdown signal
 	go func() {
